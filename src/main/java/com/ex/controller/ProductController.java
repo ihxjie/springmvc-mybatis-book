f2c22e502ba2d7b7ecf9b8c1dd08b14177d5ad09
Gribouillage <2242125522@qq.com>
2019-12-05 21:55:16 +0800

    分页查询

--- src/main/java/com/ex/controller/ProductController.java
+++ src/main/java/com/ex/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.ex.controller;
 import com.ex.dao.ProductMapper;
 import com.ex.model.Product;
 import com.ex.service.ProductService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +19,7 @@ import java.util.List;
 @RequestMapping("/product")
 public class ProductController {
     @Resource
-    ProductService productService;
+    private ProductService productService;
 
     @GetMapping("/findProduct/{keyword}")
     @ResponseBody
@@ -45,5 +48,15 @@ public class ProductController {
         productService.addProduct(product);
         System.out.println("Xjie");
         return "hello";
+    }
+
+    @GetMapping("/list")
+    public String pageList(ModelMap map,@RequestParam(defaultValue = "1",required = true,value = "pageNo")Integer pageNo){
+        Integer pageSize = 5;
+        PageHelper.startPage(pageNo, pageSize);
+        List<Product> productsList = productService.findAllProduct();
+        PageInfo<Product> pageInfo = new PageInfo<Product>(productsList);
+        map.addAttribute("pageInfo", pageInfo);
+        return "list";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Captivate
+  Date: 2019/12/5
+  Time: 16:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<a href="product/list">分页查询</a>
+
 </body>
 </html>
